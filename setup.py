69643057b30a1d4a352758d3ac69d75775554e9b
John Hopper <john.hopper@rackspace.com>
2013-06-06 14:34:10 -0500

    setup updates

--- setup.py
+++ setup.py
@@ -83,10 +83,25 @@ cythonize()
 
 setup(
     name='pylognorm',
-    version='0.1',
+    version='0.1.1',
     description='liblognorm python bindings',
+    url='http://github.com/zinic/pylognorm',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Logging',
+        'Programming Language :: Python',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3'
+    ],
     author='John Hopper',
     author_email='john.hopper@jpserver.net',
+    license='Apache 2.0',
     tests_require=read('./tools/test-requires'),
     install_requires=read('./tools/pip-requires'),
     test_suite='nose.collector',
